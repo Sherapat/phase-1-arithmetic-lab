8db8047c70154007a2e33b7a3fca39039f8987a9
sherapatrejepova <st.rejepova@gmail.com>
2021-06-27 17:05:34 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+var num1 = 31
+var num2 = 2
+var multiply = num1 * num2
 
-// Write your code here
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+
+var num3 = 100
+var num4 = 96
+var mod = num3 % num4
+
+var max = Math.max(1, 3, 20, 5, 6, 8, 10)
